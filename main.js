876d1e3bea3b4c78dd4e47c6d98bb73cde3c6a8c
giov <pintogiovanni818@gmail.com>
2021-07-21 14:57:01 +0200

    snack

---
+++ main.js
@@ -0,0 +1,32 @@
+// // Inserisci un numero, 
+// var number = parseInt(prompt("inserisci un numero"));
+// // se è pari stampa il numero,
+// if (number %2 == 0) {
+//     console.log(number);
+// }
+// //  se è dispari stampa il numero successivo
+// else {
+//     console.log(number)
+// }
+
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi,
+//  Gatsby vuole generare una falsa lista di invitati.
+
+// var names = ["Giovanni" ,"Alberto","Paolo"];
+// var lastNames = ["Rossi","Verdi","Neri"];
+
+
+// var randomInvited = names[Math.floor(Math.random()*names.length)]+" " + lastNames[Math.floor(Math.random()*lastNames.length)];
+// console.log(randomInvited);
+
+
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+var numberInteger = [1,2,3,4,5,6,7,8,9]
+var sum = 0;
+
+for (var i = 0; i < numberInteger.length; i++){
+    if (numberInteger.length %2 == 1) {
+        
+        console.log(sum)
+        }
+}
