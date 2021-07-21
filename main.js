9babfa4d613ad93dd1862a2604c436ead77d5428
giov <pintogiovanni818@gmail.com>
2021-07-21 15:25:54 +0200

    snack

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // }
 // //  se è dispari stampa il numero successivo
 // else {
-//     console.log(number)
+//     console.log(number + 1)
 // }
 
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi,
@@ -25,8 +25,8 @@ var numberInteger = [1,2,3,4,5,6,7,8,9]
 var sum = 0;
 
 for (var i = 0; i < numberInteger.length; i++){
-    if (numberInteger.length %2 == 1) {
-        
+    if (i %2 == 1) {
+        sum=sum+i;
         console.log(sum)
         }
 }
